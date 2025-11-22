bada01f193e302aa9d2b0dacb4bce0f298af7ab7
Sidharth Dhiman <sidharthdhimangurgaon@gmail.com>
2025-11-22 19:30:51 +0530

    email-service-update

--- server/src/utils/mailer.ts
+++ server/src/utils/mailer.ts
@@ -3,7 +3,9 @@ import nodemailer from "nodemailer";
 export async function sendMail(to: string, subject: string, html: string) {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
